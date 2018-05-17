0ce14421807d83f23afcc4e07432894e16917e55
Dante Medina <dmedinav3@gmail.com>
2018-05-17 15:53:40 -0400

    fix checkstyle warning 2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-   * @param someone
-   * @return
+   * @param someone es el nombre de la persona
+   * @return greeting string
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Esta es el nombre del package info
+ * Esta es el nombre del package info.
  */
 package com.example;
